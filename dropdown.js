c7fde3ec3b4af40ab32ef40c8498bf47756858ce
Ayur <ayur@MacBook-Pro-Ayur.local>
2023-03-14 10:39:51 +0300

    ASDJWkads

--- dropdown.js
+++ dropdown.js
@@ -2,9 +2,16 @@ let giveValBtn = document.querySelector('.give__valute-button');
 let giveValList = document.querySelector('.give__valute-list');
 let giveValListItems = giveValList.querySelectorAll('.give__valute-list-item')
 let giveValInp = document.querySelector('.give__valute__input-hidden');
+let typeOfValuteBtn = document.querySelector('.typeOfValute-button');
+let typeOfValuteList = document.querySelector('.typeOfValute-list');
+let typeOfValuteListItem = document.querySelectorAll('.typeOfValute-list-item');
 
 giveValBtn.addEventListener('click', function(){
     giveValList.classList.toggle('give__valute-list--visible');
+});
+
+typeOfValuteBtn.addEventListener('click', function(){
+    typeOfValuteList.classList.toggle('typeOfValute-list--visible')
 });
 
 giveValListItems.forEach(function(listItem){

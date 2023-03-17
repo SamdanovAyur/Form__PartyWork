a04d422a5067b0b41d85c41439de4d80452d9b48
SamdanovAyur <samdanov03@gmail.com>
2023-03-17 15:39:27 +0300

    qwcsjvcopoi

--- dropdown.js
+++ dropdown.js
@@ -13,52 +13,45 @@ let typeOfValuteListLtc = document.querySelector('.typeOfValute-list-litecoin');
 let typeOfValuteListItem = document.querySelectorAll('.typeOfValute-list-item');
 let typeOfValuteInpHidden = document.querySelector('.typeOfValute__input-hidden');
 
+let giveWarn = document.querySelector('.give__warning');
 let giveWarnTxt = document.querySelector('.give__warning-text');
 
 giveValBtn.addEventListener('click', function(){
     giveValList.classList.toggle('give__valute-list--visible');
 });
 
-typeOfValuteBtn.addEventListener('click', function (){
-    changeTypeOfValute().classList.toggle('typeOfValute-list--visible');
-});
-
-function changeTypeOfValute(){
-    if(giveValInpHidden.value == "bitcoin"){
-        return typeOfValuteListBtc;
-    }
-    if(giveValInpHidden.value == "tether"){
-        return typeOfValuteListTtr;
-    }
-    if(giveValInpHidden.value == "ethereum"){
-        return typeOfValuteListEtrm;
-    }
-    if(giveValInpHidden.value == "litecoin"){
-        return typeOfValuteListLtc;
-    }
-}
-
 giveValListItems.forEach(function(listItem){
-    listItem.addEventListener('click', function(){
+    listItem.addEventListener('click', function(){   
+        giveValInpHidden.value = this.dataset.value;     
         giveValBtn.innerText = this.innerText;
         giveValBtn.focus();
         giveValInpHidden.value = this.dataset.value;
         giveValList.classList.remove('give__valute-list--visible');
         changeWarning();
+        typeOfValuteBtn.innerText = activeTypeOfValute().innerText;
+        typeOfValuteInpHidden.value = activeTypeOfValute().dataset.value;
     });
 });
 
-
-
-typeOfValuteListItem.forEach(function(listItem){
-    listItem.addEventListener('click', function(){
-        typeOfValuteBtn.innerText = this.innerText;
-        typeOfValuteBtn.focus();
-        typeOfValuteInpHidden.value = this.dataset.value;
-        changeTypeOfValute().classList.remove('typeOfValute-list--visible');
-        changeWarning();
+typeOfValuteBtn.addEventListener('click', function (){
+    activeValute().classList.toggle('typeOfValute-list--visible');
+    activeValute().querySelectorAll('.typeOfValute-list-item').forEach(function(listItem){
+        listItem.addEventListener('click', function(){
+            typeOfValuteBtn.innerText = listItem.innerText;
+            typeOfValuteBtn.focus();
+            typeOfValuteInpHidden.value = listItem.dataset.value;
+            activeValute().classList.remove('typeOfValute-list--visible');
+            changeWarning();
+        });
     });
 });
+
+function activeTypeOfValute(){
+    for(let item of activeValute().querySelectorAll('.typeOfValute-list-item')){
+        return item;
+    }
+}
+
 
 document.addEventListener('click', function(e){
     if(e.target !== giveValBtn){
@@ -68,7 +61,7 @@ document.addEventListener('click', function(e){
 
 document.addEventListener('click', function(e){
     if(e.target !== typeOfValuteBtn){
-        changeTypeOfValute().classList.remove('typeOfValute-list--visible');
+        activeValute().classList.remove('typeOfValute-list--visible');
     }
 });
 
@@ -84,19 +77,48 @@ function changeWarning(){
         giveWarnTxt.textContent = 'Требуются 15 подтверждений от сети BSC.';
         adaptWarning(giveWarnTxt);
     }
+    
 
 
+    
     if(giveValInpHidden.value == "tether"){
-    giveWarnTxt.textContent = 'Пожалуйста, убедитесь, что вывод производится в рамках сети TRC-20, в противном случае, средства не зачислятся на наш кошелек. Требуется 12 подтверждений от сети.';
-    giveValInp.value = 66.27;
-    adaptWarning(giveWarnTxt);
+        giveWarnTxt.textContent = 'Пожалуйста, убедитесь, что вывод производится в рамках сети TRC-20, в противном случае, средства не зачислятся на наш кошелек. Требуется 12 подтверждений от сети.';
+        giveValInp.value = 66.27;
+        adaptWarning(giveWarnTxt);
     }
+
+    if(typeOfValuteInpHidden.value == "bep20Thr"){
+        giveWarnTxt.textContent = 'Пожалуйста, убедитесь, что вывод производится в рамках сети BEP-20, в противном случае, средства не зачислятся на наш кошелек. Требуется 12 подтверждений от сети.';
+        adaptWarning(giveWarnTxt);  
+    }
+
+    if(typeOfValuteInpHidden.value == "erc20Thr"){
+        giveWarnTxt.textContent = 'Пожалуйста, убедитесь, что вывод производится в рамках сети ERC-20, в противном случае, средства не зачислятся на наш кошелек. Требуется 12 подтверждений от сети.';
+        adaptWarning(giveWarnTxt);
+    }
+    
+    if(typeOfValuteInpHidden.value == "solThr"){
+        giveWarn.style.background = '#be3455';
+        giveWarnTxt.style.display = 'none';
+    }
+
+
+
+
     
     if(giveValInpHidden.value == "ethereum"){
         giveWarnTxt.textContent = 'Для отправки на наши кошельки используйте только сеть erc20, использование другой сети повлечет за собой потерю средств!';
         giveValInp.value = 0.03861141;
         adaptWarning(giveWarnTxt);
     }
+
+    if(typeOfValuteInpHidden.value == "bep20Eth"){
+        giveWarnTxt.textContent = 'Для отправки на наши кошельки используйте только сеть Bep-20, использование другой сети повлечет за собой потерю средств!';
+        adaptWarning(giveWarnTxt);
+    }
+
+
+    
     if(giveValInpHidden.value == "litecoin"){
         giveValInp.value = 0.77263327;
         giveWarnTxt.textContent = 'Требуется два подтверждения от сети Litecoin.';
@@ -109,4 +131,20 @@ function adaptWarning(txt){
     txt.style.margin = '0';
     txt.style.padding = '5px 0';
     txt.style.borderRadius = '5px';
+    txt.style.display = 'flex';
+}
+
+function activeValute(){
+    if(giveValInpHidden.value == "bitcoin"){
+        return typeOfValuteListBtc;
+    }
+    if(giveValInpHidden.value == "tether"){
+        return typeOfValuteListTtr;
+    }
+    if(giveValInpHidden.value == "ethereum"){
+        return typeOfValuteListEtrm;
+    }
+    if(giveValInpHidden.value == "litecoin"){
+        return typeOfValuteListLtc;
+    }
 }
